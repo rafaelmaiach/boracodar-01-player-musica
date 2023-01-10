2538734389a56faec79137d1de2563bd9798ff16
rafaelmaiach <rafael.chieregatto@hotmail.com>
2023-01-10 20:24:30 -0300

    feat: creates player container

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
+  <div class="container">
+    <main class="player">
+
+    </main>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 270px;
+  padding: 38px;
+  border-radius: 10px;
+  background-color: var(--purple);
+}
+</style>
