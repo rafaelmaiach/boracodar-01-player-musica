9a998f11bba5e1fd67b431e4a73d3e9a8968f0d9
rafaelmaiach <rafael.chieregatto@hotmail.com>
2023-01-10 20:13:31 -0300

    chore: setup initial styling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import './styles/reset.css'
+import './styles/colors.css'
+import './styles/global.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
